54516a4c457e9e25183a38125ffeb406b509be60
zakwatts <47150349+zakwatts@users.noreply.github.com>
2023-05-02 11:30:51 +0100

    0,9,12,18 forecast horizon collection

--- scripts/data_collection/Island_nwp_ext_latlon_pre20210411.py
+++ scripts/data_collection/Island_nwp_ext_latlon_pre20210411.py
@@ -238,7 +238,7 @@ class UcarDownload:
 
         while date < end_date:
             # to increase forcast add script or (3,6,9,12,...)
-            for fc in (3,6):
+            for fc in (0,9,12,18):
                 url = build_url(date, fc)
                 try:
                     file = Path(download_file(url, cookies=self.cookies))
