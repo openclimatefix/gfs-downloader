778bdfa1ecdf9e489ed4d8104ce58541e98aa977
Simon Lemieux <1105380+simlmx@users.noreply.github.com>
2023-05-04 16:01:01 -0400

    Simplify merge script

--- scripts/data_collection/merge_zarr_8step_3_to_24.py
+++ scripts/data_collection/merge_zarr_8step_3_to_24.py
@@ -1,3 +1,15 @@
+"""
+Merge step files downloaded from the other script.
+
+We assume that the files are organized as such:
+
+    /dir/step-0/*.zarr
+    /dir/step-3/*.zarr
+    ...
+
+
+FIXME better docstring
+"""
 from pathlib import Path
 
 import typer
@@ -5,61 +17,28 @@ import xarray as xr
 from tqdm import tqdm
 
 
-# Ensure no hidden files like .DS_Store are in the folder or log files!!
-# (3,6,9,12,18,24,36,48,60,72)
+def _find_zarrs_in_dir(directory: Path) -> list[Path]:
+    return [x for x in directory.iterdir() if x.suffix == '.zarr']
 
-def merge_zarrs(path: Path, out_path: Path) -> None:
-    print(f"Merging zarrs from '{path}'")
+def main(
+    path_in: Path,
+    path_out: Path,
+) -> None:
+    step_dirs = [x for x in path_in.iterdir() if x.isdir()]
+    step_datasets: list[xr.Dataset] = []
+    for step_dir in step_dirs:
+        print(f'Treating dir {step_dir}')
+        dataset_paths = _find_zarrs_in_dir(step_dir)
+        datasets = [xr.open_zarr(d) for d in dataset_paths]
+        # Merge the steps along the time dimension.
+        # FIXME make sure they are in order! This could be done by sorting on the file names and
+        # making sure that they are guaranteed to be in order.
+        merged = xr.concat(datasets, dim='time')
+        step_datasets.append(merged)
 
-    zarrs_f3 = []
-    zarrs_f6 = []
-    zarrs_f9 = []
-    zarrs_f12 = []
-    zarrs_f15 = []
-    zarrs_f18 = []
-    zarrs_f21 = []
-    zarrs_f24 = []
-    
+    # Merge all the step datasets in the step dimension
+    merged_dataset = xr.concat(step_dataset, dim='step')
 
-
-    for task_folder in tqdm(sorted(path.glob('task_*'))):
-        if task_folder.is_dir():
-            for d in task_folder.glob('*'):
-        # for d in tqdm(sorted(path.iterdir()))::
-                if d.is_dir():
-                    ds = xr.open_zarr(d)
-                    if "f003" in d.stem:
-                        zarrs_f3.append(ds)
-                    elif "f006" in d.stem:
-                        zarrs_f6.append(ds)
-                    elif "f009" in d.stem:
-                        zarrs_f9.append(ds)
-                    elif "f012" in d.stem:
-                        zarrs_f12.append(ds)
-                    elif "f015" in d.stem:
-                        zarrs_f15.append(ds)
-                    elif "f018" in d.stem:
-                        zarrs_f18.append(ds)
-                    elif "f021" in d.stem:
-                        zarrs_f21.append(ds)
-                    elif "f024" in d.stem:
-                        zarrs_f24.append(ds)
-                
-
-    
-    ds_f3 = xr.concat(zarrs_f3, dim="time")
-    ds_f6 = xr.concat(zarrs_f6, dim="time")
-    ds_f9 = xr.concat(zarrs_f9, dim="time")
-    ds_f12 = xr.concat(zarrs_f12, dim="time")
-    ds_f15 = xr.concat(zarrs_f15, dim="time")
-    ds_f18 = xr.concat(zarrs_f18, dim="time")
-    ds_f21 = xr.concat(zarrs_f21, dim="time")
-    ds_f24 = xr.concat(zarrs_f24, dim="time")
-    
-
-    ds_out = xr.concat(
-        [ds_f3, ds_f6, ds_f9, ds_f12, ds_f15, ds_f18, ds_f21, ds_f24], dim = "step"
-    )  
     print(f"Saving single zarr to '{out_path}'")
     ds_out.to_zarr(
         out_path,
@@ -68,14 +47,6 @@ def merge_zarrs(path: Path, out_path: Path) -> None:
             "valid_time": {"dtype": "int64", "_FillValue": -1},
         },
     )
-
-
-def main(
-    path_in: Path,
-    path_out: Path,
-) -> None:
-    merge_zarrs(path_in, path_out)
-
 
 if __name__ == "__main__":
     typer.run(main)
